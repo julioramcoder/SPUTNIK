ce77fec576018a3af48f2511a1ea71cd4be01b26
julioramcoder <julioramirez79@gmail.com>
2025-11-06 13:52:25 -0500

    conversion from celcius until fahrengeit

---
+++ Taller/change°Cto°F.py
@@ -0,0 +1,9 @@
+#now we are gonna change or a conversion from celcius until fahrengeit
+#ask the valor celcius
+
+celcius=float(input("please type the valor of celcius: "))
+
+#now just we leave the stipulated formula 
+conversion=(celcius*1.8)/32
+#with the command print show it the result
+print("the Result in fahrengeit: ",conversion) 
