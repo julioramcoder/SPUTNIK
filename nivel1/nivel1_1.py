a28cdca7ba6596abd9986351e3b032f605abe3ef
Cohorte 6 <antonio.pulgarin97@gmail.com>
2025-11-06 11:45:44 -0500

    nivel1_1

---
+++ nivel1/nivel1_1.py
@@ -0,0 +1,6 @@
+#Hola usuario: pide al usuario su nombre y edad. Luego imprime un mensaje: "Hola [nombre], tienes [edad] años."
+
+nombre = input("Hola usuario ¿Cuál es tu nombre? ")
+edad = int(input("¿Cuál es tu edad? "))
+
+print(f"Hola {nombre}, tienes {edad} años")
