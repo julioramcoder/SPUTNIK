de025108b35013f3c26581867780e87ce8e01a92
StivenMoscoso <stivencolombia@gmail.com>
2025-11-06 13:51:48 -0500

    Conversor_temperatura.py

--- Conversor_temperatura.py
+++ Conversor_temperatura.py
@@ -1,4 +1,4 @@
 #Conversor de grados Celsius a Fahrenheit
 celsius = float(input("Ingrese la temperatura en grados Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
+fahrenheit = (celsius * 1.8) + 32
 print(f"{celsius} grados Celsius son {fahrenheit} grados Fahrenheit.")      
