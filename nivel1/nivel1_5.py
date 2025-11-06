6cad924b846d9d2ea4e284e5dba3976230e7b730
Cohorte 6 <antonio.pulgarin97@gmail.com>
2025-11-06 12:31:52 -0500

    modificacion nivel1_5 agregué una función

--- nivel1/nivel1_5.py
+++ nivel1/nivel1_5.py
@@ -4,3 +4,10 @@ dato = input("ingresa un dato: ") #se pide el dato al usuario
 
 print(type(dato)) #se muestra qué tipo de dato es... (La verdad no entiendo bien el ejercicio, ya que las entradas siempre son tipo str,
                     # podría usar los métodos que tiene str como isalpha o isnumber, pero ya no estaría usando type())
+
+def tipoDeDato(data): #como input siempre es str, mejor hice una función
+    print(type(data))
+
+tipoDeDato(2) #ejemplo  con argumento 2, salida esperada int
+tipoDeDato(2.4) #ejemplo  con argumento 2.4, salida esperada float
+tipoDeDato("hola") #ejemplo  con argumento "hola", salida esperada str
