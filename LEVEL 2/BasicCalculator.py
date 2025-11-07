4fbf5c3afb7fc6ae12f510f2b38a58dab3d248b5
julioramcoder <julioramirez79@gmail.com>
2025-11-07 10:15:12 -0500

    i forgot to put the comments

--- LEVEL 2/BasicCalculator.py
+++ LEVEL 2/BasicCalculator.py
@@ -1,9 +1,12 @@
+#This case we are gonna identify ths typos from mathematical operation symbol 
+#firts we ask to the user 2 number with the command input
 num1=int(input("Please type a integer: "))
 num2=int(input("Please type a othe integer: "))
 
-
+#then we need to ask with what mathematical operation symbol the user want to work 
 operator = input("Please type a mathematical operation symbol: ")
 
+#with the command if, elif and else 
 if (operator == '*'):
     print("his result is: ", num1*num2 )
 elif (operator == '/'):
