d1d255e5aa7ad788ae5152e096d9d8e67b594047
julioramcoder <julioramirez79@gmail.com>
2025-11-07 06:35:12 -0500

    types of variables

---
+++ type.py
@@ -0,0 +1,12 @@
+#we are gonna show how the type command help us indetify the type of each varible 
+
+#we proceed to fix each pice of information depending on what type it is
+#using the commands str,int,float and bool
+nombre = str(input("please type your name: "))
+print(type(nombre))
+integer = int(input("please type you age: "))
+print(type(integer))
+decimal_numer = float(input("please type a distance: "))
+print(type(decimal_numer))
+falseandtrue=bool(input("are you old?: "))
+print(type(falseandtrue))
