af67eb55c683cb2ca59d09efb27d7a3f8a3281bf
StivenMoscoso <stivencolombia@gmail.com>
2025-11-07 12:37:27 -0500

    ♻️ Restaurando archivos eliminados accidentalmente (HolaUsuario, Conversor, Edad10)

--- #Conversor_temperatura.py
+++
@@ -1,4 +0,0 @@
-#Conversor de grados Celsius a Fahrenheit
-celsius = float(input("Ingrese la temperatura en grados Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print(f"{celsius} grados Celsius son {fahrenheit} grados Fahrenheit.")      
--- #HolaUsuario.py
+++
@@ -1,9 +0,0 @@
-#HolaUsuario
-
-# Este programa solicita al usuario su nombre y edad, y luego muestra la información proporcionada.
-nombre = input("Señor usuario escribe tu nombre: ") 
-edad = int(input("Señor usuario escribe tu edad: "))
-
-print("Gracias por la información")
-# Mostrar el nombre y la edad del usuario
-print(f"Su nombre es {nombre} y su edad es {edad}" )
