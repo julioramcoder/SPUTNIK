0c82f92e05e014c90de596d3e16a4625b888321b
julioramcoder <julioramirez79@gmail.com>
2025-11-07 06:53:51 -0500

    you are an adult

---
+++ LEVEL 2/AnAdult.py
@@ -0,0 +1,9 @@
+#no we get to ask to the user his age
+#in this case the way that we know how to make a condition it's with the commands if and else, we can using the next way:
+
+current_age = int(input("Please type your current age: "))
+if current_age < 18:
+    print("you are still young, don't worry so much")
+else:
+    print("you are an old man, you should start to worry about it")
+    
