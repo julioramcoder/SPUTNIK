9259be63ad496ccd333976e1a411ed5f9bd9629e
julioramcoder <julioramirez79@gmail.com>
2025-11-06 21:31:59 -0500

    know your future age

---
+++ FutureAge.py
@@ -0,0 +1,9 @@
+#know your future age
+#ask to the user to introduce his current age because we need to know this fact
+Age_actual = int(input("Please type your current age: "))
+#then just we are gonna make a mathematical function 
+age_future=(Age_actual + 10)
+#now with the command print just show the result
+print("your age in 10 years in the future will be :", age_future, "years old")
+
+ 
