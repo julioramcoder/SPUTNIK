6b45bafda604cd59dd658ba060b0c9469df1cbfd
cifuentesalfonso367-dotcom <cifuentesalfonso367@gmail.com>
2025-11-07 09:02:14 -0500

    celsius

---
+++ nivel1/celsius.py
@@ -0,0 +1,6 @@
+# tomamos la temperatura en celsius para poder realizar el facotr de conversion 
+
+celsius = float(input("Ingresa la temperatura en °C: "))
+fahrenheit = (celsius * 9/5) + 32
+
+print("La temperatura en °F es:", fahrenheit)
