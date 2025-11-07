e62a84731ae5577b405750de019cddcaaaeb1b06
julioramcoder <julioramirez79@gmail.com>
2025-11-07 13:11:37 -0500

    count

---
+++ LEVEL 3/Count.py
@@ -0,0 +1,4 @@
+#to count from 1 until 10, we need to use the command for range, like this
+
+for julio in range (1,11,1): #in this case we should make a clear, because, if we want count until 10, it's necesary put a range from 1 until 11
+    print(julio) #because the last number doesn´t count 
