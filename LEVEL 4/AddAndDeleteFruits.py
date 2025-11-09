d5572fe35af3c878d6af931a51291280a239ca12
julioramcoder <julioramirez79@gmail.com>
2025-11-08 20:46:23 -0500

    Add and delate information

---
+++ LEVEL 4/AddAndDeleteFruits.py
@@ -0,0 +1,11 @@
+#we just to build a list of diferents fruist, like this
+List = [ "apple","orange","mango", "pinapple", "bannana" ]
+#now if i want to add a new  element into my list just need to type the next:
+List.append("coco")
+print(List)
+
+#now if we want delate information from the list just we need doing the next using remove
+List_fruit = [ "apple","orange","mango", "pinapple", "bannana" ] 
+List_fruit.remove("orange")
+print(List_fruit) #and print to show us the new list 
+
