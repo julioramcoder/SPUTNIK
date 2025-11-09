499a5ee1e8c13a5a9c3e5154b7ddf9c13dad8d04
julioramcoder <julioramirez79@gmail.com>
2025-11-08 20:53:03 -0500

    looking for a fruit

---
+++ LEVEL 4/LookForInList.py
@@ -0,0 +1,7 @@
+#we are gonna look especific information in the list
+
+#we just to build a list of diferents fruist, like this
+List = [ "apple","orange","mango", "pinapple", "bannana" ]
+#if we want to find a specific fruit we just need to user print(list[#]) like this
+print(List[4])
+#we just say him the code with this number "hey show me what do you have in the space 4"
