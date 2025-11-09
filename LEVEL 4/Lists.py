90eba3b51c0eaa51460d35c7309e037decb6b143
julioramcoder <julioramirez79@gmail.com>
2025-11-08 20:31:35 -0500

    list of fruits

---
+++ LEVEL 4/Lists.py
@@ -0,0 +1,5 @@
+# list of fruit
+
+#we just to build a list of diferents fruist, like this
+List = [ "apple","orange","mango", "pinapple", "bannana" ]
+print(List) # with this command we can show all the list 
