bf544211659a1d6c1e6a28e3b19a11b3dcf7acf4
Antonio <antonio.pulgarin97@gmail.com>
2025-11-09 20:33:42 -0500

    modificación nivel4_3, ahora la función no depende de (if el in arr)

--- nivel4/nivel4_3.py
+++ nivel4/nivel4_3.py
@@ -7,4 +7,14 @@ def buscarElemento(arr,elemento):                     #como no quiero hardcodear
     else: print(f"{elemento} no se encuentra en {arr}") #si no está, también le dice al usuario que no está
 
 
+    #-----------version sin "trampa"------------------------
+
+    def BuscarElementoBien(arr,elemento):   #nueva función
+        encontrado = False                  #inicialmente, se define "encontrado" como falso
+        for i in range(len(arr) - 1):       #bucle for, desde 0 hasta n - 1       
+            if arr[i] == elemento:          #si solo un elemento de "arr" es igual a "elemento", se encontró, no hace falta seguir buscando
+                encontrado = True; break
+        return encontrado                   #se retorna el booleano encontrado (false si no se encuentra, true si sí)
+    #modifiqué la función porque no quería usar la linea: "if elemento in arr" porque es como hacer trampa, usar una función para hacer la misma función que me piden hacer
+
 
