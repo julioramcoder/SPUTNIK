334977d054d27d0fa6526725296942fd46182423
stivenmoscoso <103283733+stivenmoscoso@users.noreply.github.com>
2025-11-10 13:04:24 -0500

    Add files via upload

---
+++ Funciones/#Function.py
@@ -0,0 +1,7 @@
+#Function
+def mostrar_edad(nombre, edad):
+    print("Hola mi nombre es", nombre, "Mi edad es de:", edad,  "años")
+    for mostrar_edad in range(5):
+
+         mostrar_edad("Stiven", 33)
+
---
+++ Funciones/#parOimpar.py
@@ -0,0 +1,11 @@
+#parOimpar
+
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese otro numero: "))
+
+def paroimpar(numero1, numero2):
+        
+       if numero1 % 2 == 0:
+          print (f"el numero" {numero1}  "es par")
+       else:
+        print("el numero", numero2 "es impar")
