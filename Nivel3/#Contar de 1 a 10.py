bcdd3df2cd5c9d98b5966ea2bc4058808daceaf4
stivenmoscoso <103283733+stivenmoscoso@users.noreply.github.com>
2025-11-10 00:42:02 -0500

    nivel 3

---
+++ Nivel3/#Contar de 1 a 10.py
@@ -0,0 +1,3 @@
+#Contar de 1 a 10 
+for i in range(1, 11):
+    print(i)    
---
+++ Nivel3/#contador regresivo con while.py
@@ -0,0 +1,6 @@
+#contador regresivo con while
+Contador = 10       
+while Contador >= 0:
+    print(Contador)
+    Contador -= 1           
+    print("¡Al infinito y mas alla!") 
