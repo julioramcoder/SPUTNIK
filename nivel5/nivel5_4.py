d1d159a85ba12ce439fd9d31624deaf16d15f99f
Antonio <antonio.pulgarin97@gmail.com>
2025-11-09 20:23:55 -0500

    lista de estudiantes de riwi hamilton, TODO: clase estudiante y clase lista, para poder hacer un CRUD con cada estudiante/lista

--- nivel5/nivel5_4.py
+++ nivel5/nivel5_4.py
@@ -1,1 +1,11 @@
 #Gestión de estudiantes (mini base de datos)
+
+import json  #libreria json, para poder abrir y modificar arhivos json (lla mini base de datos)
+
+# Abrir y leer el archivo JSON
+with open("nivel5/estudiantes.json", "r") as archivo: 
+    estudiantes = json.load(archivo)
+
+
+for estudiante in estudiantes:
+    print(estudiante["nombre"])
