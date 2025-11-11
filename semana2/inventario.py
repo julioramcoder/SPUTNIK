3a62f3cd8b1f5992803b52153bb7532670c5caf2
antonio <antonio.pulgarin97@gmail.com>
2025-11-11 07:39:35 -0500

    terminadas task 1,2,3 y 4. es necesario integrar todo en la task 5

--- semana2/inventario.py
+++ semana2/inventario.py
@@ -22,7 +22,7 @@ while True:
         break
     except ValueError:
         print("Ingresa una opción válida")
-print(menu)
+
 
 #--------------------------Task 2-----------------------------------
 """
@@ -32,3 +32,76 @@ Permite agregar varios productos seguidos (nombre, precio y cantidad).
 Cada producto debe almacenarse como un diccionario dentro de una lista llamada inventario.
 producto = {"nombre": "Lápiz", "precio": 500, "cantidad": 3}
 inventario.append(producto)"""
+
+inventario = []
+
+def createNewItem(_nombre,_precio,_cantidad):
+    newItem = dict(nombre = _nombre, precio = _precio, cantidad =_cantidad)
+    return newItem
+
+def addNewItem(_nombre,_precio,_cantidad):
+    inventario.append(createNewItem(_nombre,_precio,_cantidad))
+
+#-----------------Task 3------------------------------------------
+"""
+Mostrar todos los productos del inventario:
+Implementa una opción en el menú que recorra el inventario con un bucle for.
+Muestra todos los productos en un formato claro:
+Producto: Lápiz | Precio: 500 | Cantidad: 3
+Si el inventario está vacío, muestra un mensaje que lo indique."""
+
+def showItems():
+    if not inventario:
+        print("Aun no hay productos en el inventario")
+    else:
+        print(f"{'Producto':<25} {'Precio':<10} {'Cantidad':<10}")
+        print("-" * 45)
+        for item in inventario:
+            print(f"{item["nombre"]:<25} {item["precio"]:<10.2f} {item["cantidad"]:<10}")
+            
+
+addNewItem("coco",2,4)
+addNewItem("cosco",2,4)
+addNewItem("co4co",2,4)
+addNewItem("coc2o",2,4)
+showItems()
+
+#----------------Task 4------------------------------------------
+"""
+ Calcular estadísticas básicas:
+Implementa en el menú una opción para calcular:
+El valor total del inventario (sumatoria de precio x cantidad).
+La cantidad total de productos registrados.
+Muestra los resultados usando print() de manera clara."""
+
+def totalItem(item):
+    cantidad = item["cantidad"]
+    precio = item["precio"]
+    return cantidad*precio
+
+def cantidadTotal():
+    total = 0
+    for item in inventario:
+        total += item["cantidad"]
+    print(f"La cantidad total de productos es: ") #aqui tengo que solucionar la ambiguedad de si es productos unitarios, sin repetir, etc
+    return total
+
+for item in inventario:
+    print(f"el total de {item["nombre"]} es: {totalItem(item)}")
+
+print(cantidadTotal())
+
+#-------------Task 5----------------------------------------------
+"""
+Documentación y limpieza del código:
+Comenta el código explicando la funcionalidad de cada sección (menú, bucle, validación, estadísticas).
+Estructura el código en funciones simples:
+agregar_producto()
+mostrar_inventario()
+calcular_estadisticas()
+Deja un comentario final resumiendo el objetivo de la semana."""
+
+
+
+
+    
