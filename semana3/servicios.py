fff01f7b5f80b3d7170c8418322f06dafc502ad9
antonio <antonio.pulgarin97@gmail.com>
2025-11-19 07:48:03 -0500

    agregadas funciones para cargar y guardar csv

--- semana3/servicios.py
+++ semana3/servicios.py
@@ -17,14 +17,25 @@ Documenta cada función con docstring (qué hace, parámetros, retorno) y agrega
 import csv
 def loadInventario() -> list[dict]:
     try:
-        with open("inventario.csv", "r") as file:
+        with open("inventario.csv", "r", encoding="utf-8") as file:
             reader = csv.DictReader(file)
             inventario = list(reader)
     except:
         inventario = []
     return inventario
 
-def saveInventario():
-    with open("inventario.csv", "w") as file:
-        return
-    
+
+def saveInventario(inventario : list[dict]):
+    try:
+        with open("inventario.csv", "w", newline="", encoding="utf-8") as file:
+            if inventario:
+                fields = inventario[0].keys()
+                writer = csv.DictWriter(file, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows(inventario)
+    except: print("error al guardar inventario")
+
+
+
+
+
